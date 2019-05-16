88eef4deefea1827efd2be53d3515d09401878cb
ThaDocIzN <rodan72@msn.com>
2019-05-16 11:49:50 -0500

    cleanup

--- app/src/main/java/com/thadocizn/plantstore/PlantDatabase.kt
+++ app/src/main/java/com/thadocizn/plantstore/PlantDatabase.kt
@@ -35,48 +35,4 @@ abstract class PlantDatabase : RoomDatabase() {
         }
 
     }
-
-    private class InitialDataAsyncTask(PlantDatabase: PlantDatabase) : AsyncTask<Void, Void, Void>() {
-        private val categoryDAO: CategoryDAO = PlantDatabase.categoryDAO()
-        private val plantDAO: PlantDAO = PlantDatabase.plantDAO()
-
-        override fun doInBackground(vararg voids: Void): Void? {
-
-            val category1 = Category(1, "House", "fjsjhfskj")
-            val category2 = Category(2, "Exotic", "fjsjhfskj")
-            val category3 = Category(3, "Desert", "fjsjhfskj")
-
-
-            categoryDAO.insert(category1)
-            categoryDAO.insert(category2)
-            categoryDAO.insert(category3)
-
-            val plant1  = Plant(1, "Plant", 2)
-            val plant2  = Plant(2, "Plant", 2)
-            val plant3  = Plant(3, "Plant", 2)
-            val plant4  = Plant(4, "Plant", 1)
-            val plant5  = Plant(5, "Plant", 3)
-            val plant6  = Plant(6, "Plant", 3)
-            val plant7  = Plant(7, "Plant", 1)
-            val plant8  = Plant(8, "Plant", 3)
-            val plant9  = Plant(9, "Plant", 1)
-            val plant10 = Plant(10, "Plant", 2)
-            val plant11 = Plant(11, "Plant", 3)
-
-            plantDAO.insert(plant1)
-            plantDAO.insert(plant2)
-            plantDAO.insert(plant3)
-            plantDAO.insert(plant4)
-            plantDAO.insert(plant5)
-            plantDAO.insert(plant6)
-            plantDAO.insert(plant7)
-            plantDAO.insert(plant8)
-            plantDAO.insert(plant9)
-            plantDAO.insert(plant10)
-            plantDAO.insert(plant11)
-
-                return null
-        }
-    }
-
 }
