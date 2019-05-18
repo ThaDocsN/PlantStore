1352c579fa44fc3b5432467d046074688d4fd7d9
ThaDocsN <charles.tha.doc@gamil.com>
2019-05-18 04:40:03 -0500

    updated repo for categories

--- app/src/main/java/com/thadocizn/plantstore/models/CategoryRepository.kt
+++ app/src/main/java/com/thadocizn/plantstore/models/CategoryRepository.kt
@@ -1,14 +1,18 @@
 package com.thadocizn.plantstore.models
 
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 
 class CategoryRepository(val dao: CategoryDAO) {
 
     val categories: LiveData<List<Category>> = dao.getAllCategories()
 
+    @WorkerThread
     fun inserCategory(category: Category) = dao.insert(category)
 
+    @WorkerThread
     fun updateCategory(category: Category) = dao.update(category)
 
+    @WorkerThread
     fun deleteCategory(category: Category) = dao.update(category)
 }
